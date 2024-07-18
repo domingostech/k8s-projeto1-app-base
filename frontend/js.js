32216d71553c0659dbd5b2232ddf60786b71e569
domingostech <42682161+domingostech@users.noreply.github.com>
2024-07-17 22:19:12 -0300

    Arquivos frontend

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "",
+        url: "http://10.101.175.81",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
